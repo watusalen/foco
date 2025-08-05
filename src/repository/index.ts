39e7688439ed3a408dc017f66c4e10f5f1f7355a
Matusalen Alves <matusalencosta@icloud.com>
2025-08-05 20:45:24 -0300

    feat(repository): adiciona exportações centralizadas de todos os repositories

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,3 +1,17 @@
 /**
  * Pasta repository: responsável por implementar o acesso e manipulação dos dados no banco.
  */
+
+// Interfaces e classes base
+export { BaseRepositoryInterface } from './BaseRepository';
+export { SupabaseBaseRepository } from './SupabaseBaseRepository';
+
+// Repositories específicos
+export { UsuarioRepository } from './UsuarioRepository';
+export { CronogramaRepository } from './CronogramaRepository';
+export { AtividadeRepository } from './AtividadeRepository';
+export { MetaRepository } from './MetaRepository';
+export { ProgressoRepository } from './ProgressoRepository';
+export { QuizRepository } from './QuizRepository';
+export { QuestaoRepository } from './QuestaoRepository';
+export { RespostaRepository } from './RespostaRepository';
